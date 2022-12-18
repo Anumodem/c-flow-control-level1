7c2c5bd2a6fe35c22353a31079fff57832fb1948
Anumodem <110585951+Anumodem@users.noreply.github.com>
2022-12-18 18:33:57 +0530

    c

---
+++ question7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    char ID;
+    scanf("%c",&ID);
+    if
+    (ID=='B'||ID=='b')
+    printf("BattleShip");
+    else if 
+    (ID=='C'||ID=='c')
+    printf("Cruiser");
+    else if (ID=='D'||ID=='d')
+    printf("Destroyer");
+    else
+    printf("Frigate");
+  	return 0;}
