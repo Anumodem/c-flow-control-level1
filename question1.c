8856877c783d61d4294b0c06c869d8737b096b89
Anumodem <110585951+Anumodem@users.noreply.github.com>
2022-12-18 18:31:08 +0530

    c

---
+++ question1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{   int A,B;
+    int final;
+    scanf("%d %d",&A,&B);
+    final=(A-2*B);
+    if(A<B)
+    {printf("%d",final);}
+    else {
+        printf("%d",final);}
+    
+      	return 0;}
