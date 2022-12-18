6fc69c1b9ce94eb9f96b9cc133511c21209497ac
Anumodem <110585951+Anumodem@users.noreply.github.com>
2022-12-18 18:32:35 +0530

    c

---
+++ question4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{int number1,number2;
+ scanf("%d %d",&number1,&number2);
+    if(number1>number2){
+    printf(">");}
+    else if (number1<number2)
+    {printf("<");
+    } else 
+    {printf("=");
+}
+	return 0;}
